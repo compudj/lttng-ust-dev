9df613f41e926fab5d3899321272fff11d774243
Francis Deslauriers <francis.deslauriers@efficios.com>
2019-11-28 14:15:02 -0500

    Decouple `struct lttng_session` from filter code
    Use `struct lttng_ctx` directly so that the future trigger
    infrastructure, which will be outside any session, can use it.
    
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
    Change-Id: I84109a0c01636bc5206f43e7abdceee27124c520

--- liblttng-ust/lttng-filter-interpreter.c
+++ liblttng-ust/lttng-filter-interpreter.c
@@ -369,7 +369,7 @@ static int context_get_index(struct lttng_ctx *ctx,
 	return 0;
 }
 
-static int dynamic_get_index(struct lttng_session *session,
+static int dynamic_get_index(struct lttng_ctx *ctx,
 		struct bytecode_runtime *runtime,
 		uint64_t index, struct estack_entry *stack_top)
 {
@@ -436,9 +436,6 @@ static int dynamic_get_index(struct lttng_session *session,
 	case LOAD_ROOT_CONTEXT:
 	case LOAD_ROOT_APP_CONTEXT:	/* Fall-through */
 	{
-		struct lttng_ctx *ctx;
-
-		ctx = rcu_dereference(session->ctx);
 		ret = context_get_index(ctx,
 				&stack_top->u.ptr,
 				gid->ctx_index);
@@ -633,7 +630,7 @@ uint64_t lttng_filter_interpret_bytecode(void *filter_data,
 		const char *filter_stack_data)
 {
 	struct bytecode_runtime *bytecode = filter_data;
-	struct lttng_session *session = bytecode->p.session;
+	struct lttng_ctx *ctx = rcu_dereference(*bytecode->p.pctx);
 	void *pc, *next_pc, *start_pc;
 	int ret = -EINVAL;
 	uint64_t retval = 0;
@@ -2012,13 +2009,11 @@ uint64_t lttng_filter_interpret_bytecode(void *filter_data,
 		{
 			struct load_op *insn = (struct load_op *) pc;
 			struct field_ref *ref = (struct field_ref *) insn->data;
-			struct lttng_ctx *ctx;
 			struct lttng_ctx_field *ctx_field;
 			struct lttng_ctx_value v;
 
 			dbg_printf("get context ref offset %u type dynamic\n",
 				ref->offset);
-			ctx = rcu_dereference(session->ctx);
 			ctx_field = &ctx->fields[ref->offset];
 			ctx_field->get_value(ctx_field, &v);
 			estack_push(stack, top, ax, bx, ax_t, bx_t);
@@ -2062,13 +2057,11 @@ uint64_t lttng_filter_interpret_bytecode(void *filter_data,
 		{
 			struct load_op *insn = (struct load_op *) pc;
 			struct field_ref *ref = (struct field_ref *) insn->data;
-			struct lttng_ctx *ctx;
 			struct lttng_ctx_field *ctx_field;
 			struct lttng_ctx_value v;
 
 			dbg_printf("get context ref offset %u type string\n",
 				ref->offset);
-			ctx = rcu_dereference(session->ctx);
 			ctx_field = &ctx->fields[ref->offset];
 			ctx_field->get_value(ctx_field, &v);
 			estack_push(stack, top, ax, bx, ax_t, bx_t);
@@ -2091,13 +2084,11 @@ uint64_t lttng_filter_interpret_bytecode(void *filter_data,
 		{
 			struct load_op *insn = (struct load_op *) pc;
 			struct field_ref *ref = (struct field_ref *) insn->data;
-			struct lttng_ctx *ctx;
 			struct lttng_ctx_field *ctx_field;
 			struct lttng_ctx_value v;
 
 			dbg_printf("get context ref offset %u type s64\n",
 				ref->offset);
-			ctx = rcu_dereference(session->ctx);
 			ctx_field = &ctx->fields[ref->offset];
 			ctx_field->get_value(ctx_field, &v);
 			estack_push(stack, top, ax, bx, ax_t, bx_t);
@@ -2112,13 +2103,11 @@ uint64_t lttng_filter_interpret_bytecode(void *filter_data,
 		{
 			struct load_op *insn = (struct load_op *) pc;
 			struct field_ref *ref = (struct field_ref *) insn->data;
-			struct lttng_ctx *ctx;
 			struct lttng_ctx_field *ctx_field;
 			struct lttng_ctx_value v;
 
 			dbg_printf("get context ref offset %u type double\n",
 				ref->offset);
-			ctx = rcu_dereference(session->ctx);
 			ctx_field = &ctx->fields[ref->offset];
 			ctx_field->get_value(ctx_field, &v);
 			estack_push(stack, top, ax, bx, ax_t, bx_t);
@@ -2204,7 +2193,7 @@ uint64_t lttng_filter_interpret_bytecode(void *filter_data,
 			struct get_index_u16 *index = (struct get_index_u16 *) insn->data;
 
 			dbg_printf("op get index u16\n");
-			ret = dynamic_get_index(session, bytecode, index->index, estack_ax(stack, top));
+			ret = dynamic_get_index(ctx, bytecode, index->index, estack_ax(stack, top));
 			if (ret)
 				goto end;
 			estack_ax_v = estack_ax(stack, top)->u.v;
@@ -2219,7 +2208,7 @@ uint64_t lttng_filter_interpret_bytecode(void *filter_data,
 			struct get_index_u64 *index = (struct get_index_u64 *) insn->data;
 
 			dbg_printf("op get index u64\n");
-			ret = dynamic_get_index(session, bytecode, index->index, estack_ax(stack, top));
+			ret = dynamic_get_index(ctx, bytecode, index->index, estack_ax(stack, top));
 			if (ret)
 				goto end;
 			estack_ax_v = estack_ax(stack, top)->u.v;
--- liblttng-ust/lttng-filter-specialize.c
+++ liblttng-ust/lttng-filter-specialize.c
@@ -502,7 +502,7 @@ static int specialize_load_object(const struct lttng_event_field *field,
 	return 0;
 }
 
-static int specialize_context_lookup(struct lttng_session *session,
+static int specialize_context_lookup(struct lttng_ctx *ctx,
 		struct bytecode_runtime *runtime,
 		struct load_op *insn,
 		struct vstack_load *load)
@@ -513,11 +513,11 @@ static int specialize_context_lookup(struct lttng_session *session,
 	struct filter_get_index_data gid;
 	ssize_t data_offset;
 
-	idx = specialize_context_lookup_name(session->ctx, runtime, insn);
+	idx = specialize_context_lookup_name(ctx, runtime, insn);
 	if (idx < 0) {
 		return -ENOENT;
 	}
-	ctx_field = &session->ctx->fields[idx];
+	ctx_field = &ctx->fields[idx];
 	field = &ctx_field->event_field;
 	ret = specialize_load_object(field, load, true);
 	if (ret)
@@ -536,7 +536,7 @@ static int specialize_context_lookup(struct lttng_session *session,
 	return 0;
 }
 
-static int specialize_app_context_lookup(struct lttng_session *session,
+static int specialize_app_context_lookup(struct lttng_ctx *ctx,
 		struct bytecode_runtime *runtime,
 		struct load_op *insn,
 		struct vstack_load *load)
@@ -559,19 +559,18 @@ static int specialize_app_context_lookup(struct lttng_session *session,
 	}
 	strcpy(name, "$app.");
 	strcat(name, orig_name);
-	idx = lttng_get_context_index(session->ctx, name);
+	idx = lttng_get_context_index(ctx, name);
 	if (idx < 0) {
 		assert(lttng_context_is_app(name));
 		ret = lttng_ust_add_app_context_to_ctx_rcu(name,
-				&session->ctx);
+				&ctx);
 		if (ret)
 			return ret;
-		idx = lttng_get_context_index(session->ctx,
-			name);
+		idx = lttng_get_context_index(ctx, name);
 		if (idx < 0)
 			return -ENOENT;
 	}
-	ctx_field = &session->ctx->fields[idx];
+	ctx_field = &ctx->fields[idx];
 	field = &ctx_field->event_field;
 	ret = specialize_load_object(field, load, true);
 	if (ret)
@@ -676,7 +675,7 @@ int lttng_filter_specialize_bytecode(struct lttng_event *event,
 	int ret = -EINVAL;
 	struct vstack _stack;
 	struct vstack *stack = &_stack;
-	struct lttng_session *session = bytecode->p.session;
+	struct lttng_ctx *ctx = *bytecode->p.pctx;
 
 	vstack_init(stack);
 
@@ -1403,7 +1402,7 @@ int lttng_filter_specialize_bytecode(struct lttng_event *event,
 				goto end;
 			case LOAD_ROOT_CONTEXT:
 				/* Lookup context field. */
-				ret = specialize_context_lookup(session,
+				ret = specialize_context_lookup(ctx,
 					bytecode, insn,
 					&vstack_ax(stack)->load);
 				if (ret)
@@ -1411,7 +1410,7 @@ int lttng_filter_specialize_bytecode(struct lttng_event *event,
 				break;
 			case LOAD_ROOT_APP_CONTEXT:
 				/* Lookup app context field. */
-				ret = specialize_app_context_lookup(session,
+				ret = specialize_app_context_lookup(ctx,
 					bytecode, insn,
 					&vstack_ax(stack)->load);
 				if (ret)
